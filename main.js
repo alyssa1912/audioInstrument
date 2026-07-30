b2304552b8fa37f7a9b986bf4232a3218f54c3b9
Alyssa Pramod <s4177012@student.rmit.edu.au>
2026-07-30 12:17:57 +1000

    added notes

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
-document.body.style.backgroundColor = "red";
-console.log("Background color changed to red");
+// document.body.style.backgroundColor = "red";
+// find my test button
+const testButton = document.getElementById("test-button");
+
+//create instrument and connect to audio
+const synth = new Tone.Synth().toDestination();
+
+// do smth when we click the button
+testButton.addEventListener("click", playTestNote);
+
+function playTestNote() {
+    synth.triggerAttackRelease("C4", "8n");
+}
