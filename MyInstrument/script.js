b3eaf76316debcdd8af73b76b49cc6df15c1dd24
Alyssa Pramod <s4177012@student.rmit.edu.au>
2026-09-10 10:46:47 +1000

    edit

--- MyInstrument/script.js
+++ MyInstrument/script.js
@@ -1,12 +1,24 @@
+
+const snapSound = new Audio("sounds/Asnap.mp3");
+
+const snapSound = new Audio("sounds/Asnap.mp3");
+
 document.addEventListener("keydown", function(event) {
 
     // Check if the physical keyboard key pressed is A
     if (event.key.toLowerCase() === "a") {
 
-        const snapSound = new Audio("sounds/Asnap.mp3");
+        snapSound.play();
+    }
+
+
+    if (event.key.toLowerCase() === "b") {
 
         snapSound.play();
-
     }
 
 });
+
+
+// two different areas for imporvement, 1. the aesthetic & visuals 2. randomness.
+// create three branches for each improvement, meaning total 6 branches.
