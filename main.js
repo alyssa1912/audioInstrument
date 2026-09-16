2cdde9e53731b843b03b40d81b65afbb5c112dc2
Alyssa Pramod <s4177012@student.rmit.edu.au>
2026-09-16 13:16:24 +1000

    fixing code
    getting rid of any unwanted & extra code not related to my project

--- main.js
+++ main.js
@@ -1,21 +1,33 @@
+// MODAL ------------------------- 
 
-// document.body.style.backgroundColor = "red";
 
-// find my test button
-const testButton = document.getElementById("test-button");
-// find my key test button
-const key = document.getElementById("key-test");
+// show modal on page load
+introModal.showModal();
+
+// when OK clicked, modal closes
+introModalCloseButton.addEventListener("click", function closeIntroModal(){
+
+    introModal.close();
+});
+
+
+
+
+// find my test button              const testButton = document.getElementById("test-button");
+// find my key test button          const key = document.getElementById("key-test");
+
+
+
 // find our intro modal
 const introModal = document.getElementById("intro-modal");
 // console.log(introModal);
+
 // find modal close button
 const introModalCloseButton = document.getElementById("intro-modal-close");
 
-// is the mouse button held?
-// let mouseButtonDown = false;
+// is the mouse button held?        let mouseButtonDown = false;
 
 //introdialog.showModal();
-//document.body.style.backgroundColor = "red";
 
 ////// Modal
 // browser loads html 
@@ -30,36 +42,18 @@ introModalCloseButton.addEventListener("click", function closeIntroModal(){
 });
 
 // when dialog closes by whatever means, load audio system
-introModal.addEventListener("close", Tone.start());
+introModal.addEventListener("close", Tone.start);
 
 // introModalCloseButton.addEventListener("click", () => {
 //    introModal.close();
 // });
 
 
-////// Tone
-
-// create instrument
-const synth = new Tone.Synth();
-
-function toneInit(){
-    // connect synth to audio output
-    synth.connect(Tone.Destination);
-}
-
-function playNote(e){
-    // find the element that the event ran on
-    let keyPressed = e.target;
-    console.log(keyPressed);
-    // find the data-note attribute of that element
-    let note = keyPressed.dataset.note;
-    console.log(note);
-}
-
-testButton.addEventListener("mousedown", playNote);
-key.addEventListener("mousedown", playNote);
 
 
+
+
+// This is the instrument ------------------------ //
 
 const snapSound = new Audio("sounds/Asnap.mp3");
 
